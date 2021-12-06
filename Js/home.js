31e88222049616b0dffed634fab00da93f576c57
Osama Khader <osama10kh@gmail.com>
2021-12-06 16:45:41 +0200

    add sixth section (pricing)

---
+++ Js/home.js
@@ -0,0 +1,20 @@
+
+const leftBtn = document.querySelector('.leftBtn');
+const rightBtn = document.querySelector('.rightBtn');
+
+const img1 = document.querySelector('.img1');
+const img2 = document.querySelector('.img2');
+const img3 = document.querySelector('.img3');
+
+const bollet1 = document.querySelector('.bollet1');
+const bollet2 = document.querySelector('.bollet2');
+const bollet3 = document.querySelector('.bollet3');
+
+
+// rightBtn.addEventListener('click', function (ev) {
+//     img1.style.display = 'none';
+//     img3.style.display = 'block';
+// });
+
+
+
